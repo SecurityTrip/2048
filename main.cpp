8993ef2f23bd807539ff931e2ddfa274325da008
Илья Лысов <51211214+SecurityTrip@users.noreply.github.com>
2023-03-04 17:20:12 +0400

    Fixed #include`s Bugs on finishing game

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <conio.h>
-#include <stdio.h>
-#include "time.h"
+#include <ctime>
 #include<bits/stdc++.h>
-#include<conio.h>
+
 
 
 #define KEY_UP 72
